3db1dca99b0ee91e1cb76b6de8af66d0c0aa7c10
Taylor Brown <4754387+skylon07@users.noreply.github.com>
2023-09-26 15:10:39 -0600

    Add UserService (with stuff ready to use for FollowingFragment)

---
+++ app/src/main/java/edu/byu/cs/tweeter/client/model/service/UserService.java
@@ -0,0 +1,59 @@
+package edu.byu.cs.tweeter.client.model.service;
+
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+
+import androidx.annotation.NonNull;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import edu.byu.cs.tweeter.model.domain.AuthToken;
+import edu.byu.cs.tweeter.client.backgroundTask.GetUserTask;
+import edu.byu.cs.tweeter.model.domain.User;
+
+public class UserService {
+    public void loadUser(AuthToken authToken, String userName, Observer observer) {
+        GetUserTask getUserTask = new GetUserTask(
+            authToken,
+            userName,
+            new GetUserHandler(observer)
+        );
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        executor.execute(getUserTask);
+    }
+
+    public interface Observer {
+        void onUserLoaded(User user);
+        void displayError(String message);
+        void displayException(Exception ex);
+    }
+
+    /**
+     * Message handler (i.e., observer) for GetUserTask.
+     */
+    private class GetUserHandler extends Handler {
+        private final Observer observer;
+
+        public GetUserHandler(Observer observer) {
+            super(Looper.getMainLooper());
+            this.observer = observer;
+        }
+
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            boolean success = msg.getData().getBoolean(GetUserTask.SUCCESS_KEY);
+            if (success) {
+                User user = (User) msg.getData().getSerializable(GetUserTask.USER_KEY);
+                observer.onUserLoaded(user);
+            } else if (msg.getData().containsKey(GetUserTask.MESSAGE_KEY)) {
+                String message = msg.getData().getString(GetUserTask.MESSAGE_KEY);
+                observer.displayError(message);
+            } else if (msg.getData().containsKey(GetUserTask.EXCEPTION_KEY)) {
+                Exception ex = (Exception) msg.getData().getSerializable(GetUserTask.EXCEPTION_KEY);
+                observer.displayException(ex);
+            }
+        }
+    }
+}
