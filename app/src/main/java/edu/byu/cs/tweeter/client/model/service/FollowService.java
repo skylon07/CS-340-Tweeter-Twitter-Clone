317a657fd7433dd39e32a350583156e81b6a9ed0
Taylor Brown <4754387+skylon07@users.noreply.github.com>
2023-09-26 15:09:33 -0600

    Add FollowService (with stuff ready to use for FollowingFragment)

---
+++ app/src/main/java/edu/byu/cs/tweeter/client/model/service/FollowService.java
@@ -0,0 +1,63 @@
+package edu.byu.cs.tweeter.client.model.service;
+
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+
+import androidx.annotation.NonNull;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import edu.byu.cs.tweeter.model.domain.AuthToken;
+import edu.byu.cs.tweeter.model.domain.User;
+import edu.byu.cs.tweeter.client.backgroundTask.GetFollowingTask;
+
+public class FollowService {
+    public void loadMoreItems(AuthToken currUserAuthToken, User user, int pageSize, User lastFollowee, Observer observer) {
+        GetFollowingTask getFollowingTask = new GetFollowingTask(
+            currUserAuthToken,
+            user,
+            pageSize,
+            lastFollowee,
+            new GetFollowingHandler(observer)
+        );
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        executor.execute(getFollowingTask);
+    }
+
+    public interface Observer {
+        void onItemsLoaded(List<User> items, boolean hasMorePages);
+        void displayError(String message);
+        void displayException(Exception ex);
+    }
+
+    /**
+     * Message handler (i.e., observer) for GetFollowingTask.
+     */
+    private class GetFollowingHandler extends Handler {
+        private final Observer observer;
+
+        public GetFollowingHandler(Observer observer) {
+            super(Looper.getMainLooper());
+            this.observer = observer;
+        }
+
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            boolean success = msg.getData().getBoolean(GetFollowingTask.SUCCESS_KEY);
+            if (success) {
+                List<User> followees = (List<User>) msg.getData().getSerializable(GetFollowingTask.FOLLOWEES_KEY);
+                boolean hasMorePages = msg.getData().getBoolean(GetFollowingTask.MORE_PAGES_KEY);
+                observer.onItemsLoaded(followees, hasMorePages);
+            } else if (msg.getData().containsKey(GetFollowingTask.MESSAGE_KEY)) {
+                String message = msg.getData().getString(GetFollowingTask.MESSAGE_KEY);
+                observer.displayError(message);
+            } else if (msg.getData().containsKey(GetFollowingTask.EXCEPTION_KEY)) {
+                Exception ex = (Exception) msg.getData().getSerializable(GetFollowingTask.EXCEPTION_KEY);
+                observer.displayException(ex);
+            }
+        }
+    }
+}
