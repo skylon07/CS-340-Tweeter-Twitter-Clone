24cb161fde8c12df7e7bb06d121d22ac9b3532db
Taylor Brown <4754387+skylon07@users.noreply.github.com>
2023-09-27 15:07:25 -0600

    Refactor FollowersFragment; Implement a Presenter

---
+++ app/src/main/java/edu/byu/cs/tweeter/client/presenter/main/followers/FollowersPresenter.java
@@ -0,0 +1,95 @@
+package edu.byu.cs.tweeter.client.presenter.main.followers;
+
+import java.util.List;
+
+import edu.byu.cs.tweeter.client.cache.Cache;
+import edu.byu.cs.tweeter.client.model.service.FollowService;
+import edu.byu.cs.tweeter.client.model.service.UserService;
+import edu.byu.cs.tweeter.model.domain.AuthToken;
+import edu.byu.cs.tweeter.model.domain.User;
+
+public class FollowersPresenter {
+    private static final int PAGE_SIZE = 10;
+
+    private User lastFollower;
+    private boolean hasMorePages;
+    private boolean isLoading;
+
+    private final View view;
+    private final FollowService followService = new FollowService();
+    private final UserService userService = new UserService();
+
+    public FollowersPresenter(View view) {
+        this.view = view;
+    }
+
+    public void onFollowerItemClick(String userName) {
+        loadUserProfile(userName);
+    }
+
+    public void loadUserProfile(String userName) {
+        view.displayMessage("Getting user's profile...");
+        AuthToken authToken = Cache.getInstance().getCurrUserAuthToken();
+        userService.loadUser(authToken, userName, new UserServiceObserver());
+    }
+
+    public void loadMoreItems(User user) {
+        if (!isLoading) {   // This guard is important for avoiding a race condition in the scrolling code.
+            isLoading = true;
+            view.setLoadingFooterVisible(true);
+            AuthToken authToken = Cache.getInstance().getCurrUserAuthToken();
+            followService.loadMoreItems(authToken, user, PAGE_SIZE, lastFollower, new FollowServiceObserver());
+        }
+    }
+
+    public interface View {
+        void addFollowers(List<User> followers);
+        void setLoadingFooterVisible(boolean visible);
+        void setCurrentUser(User user);
+        void displayMessage(String message);
+    }
+
+    private class FollowServiceObserver implements FollowService.Observer {
+        @Override
+        public void onItemsLoaded(List<User> followees, boolean hasMorePages) {
+            isLoading = false;
+            view.setLoadingFooterVisible(false);
+            FollowersPresenter.this.hasMorePages = hasMorePages;
+            lastFollower = (followees.size() > 0) ? followees.get(followees.size() - 1) : null;
+            view.addFollowers(followees);
+        }
+
+        @Override
+        public void displayError(String message) {
+            isLoading = false;
+            view.setLoadingFooterVisible(false);
+
+            view.displayMessage("Failed to get follower: " + message);
+        }
+
+        @Override
+        public void displayException(Exception ex) {
+            isLoading = false;
+            view.setLoadingFooterVisible(false);
+
+            view.displayMessage("Failed to get follower because of exception: " + ex.getMessage());
+        }
+    }
+
+    private class UserServiceObserver implements UserService.Observer {
+        @Override
+        public void onUserLoaded(User user) {
+            view.setCurrentUser(user);
+        }
+
+        @Override
+        public void displayError(String message) {
+            view.displayMessage("Failed to get user: " + message);
+        }
+
+        @Override
+        public void displayException(Exception ex) {
+            view.displayMessage("Failed to get user because of exception: " + ex.getMessage());
+        }
+    }
+}
--- app/src/main/java/edu/byu/cs/tweeter/client/view/main/followers/FollowersFragment.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/view/main/followers/FollowersFragment.java
@@ -31,13 +31,15 @@ import edu.byu.cs.tweeter.R;
 import edu.byu.cs.tweeter.client.cache.Cache;
 import edu.byu.cs.tweeter.client.model.service.backgroundTask.GetFollowersTask;
 import edu.byu.cs.tweeter.client.model.service.backgroundTask.GetUserTask;
+import edu.byu.cs.tweeter.client.presenter.main.followers.FollowersPresenter;
 import edu.byu.cs.tweeter.client.view.main.MainActivity;
+import edu.byu.cs.tweeter.model.domain.Status;
 import edu.byu.cs.tweeter.model.domain.User;
 
 /**
  * Implements the "Followers" tab.
  */
-public class FollowersFragment extends Fragment {
+public class FollowersFragment extends Fragment implements FollowersPresenter.View {
 
     private static final String LOG_TAG = "FollowersFragment";
     private static final String USER_KEY = "UserKey";
@@ -45,11 +47,10 @@ public class FollowersFragment extends Fragment {
     private static final int LOADING_DATA_VIEW = 0;
     private static final int ITEM_VIEW = 1;
 
-    private static final int PAGE_SIZE = 10;
-
     private User user;
 
     private FollowersRecyclerViewAdapter followersRecyclerViewAdapter;
+    private FollowersPresenter presenter;
 
     /**
      * Creates an instance of the fragment and places the target user in an arguments
@@ -71,6 +72,8 @@ public class FollowersFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        presenter = new FollowersPresenter(this);
+
         View view = inflater.inflate(R.layout.fragment_followers, container, false);
 
         //noinspection ConstantConditions
@@ -82,11 +85,38 @@ public class FollowersFragment extends Fragment {
         followersRecyclerView.setLayoutManager(layoutManager);
 
         followersRecyclerViewAdapter = new FollowersRecyclerViewAdapter();
+        followersRecyclerViewAdapter.loadMoreItems();
         followersRecyclerView.setAdapter(followersRecyclerViewAdapter);
 
         followersRecyclerView.addOnScrollListener(new FollowRecyclerViewPaginationScrollListener(layoutManager));
 
         return view;
+    }
+
+    @Override
+    public void addFollowers(List<User> followers) {
+        followersRecyclerViewAdapter.addItems(followers);
+    }
+
+    @Override
+    public void setLoadingFooterVisible(boolean visible) {
+        if (visible) {
+            followersRecyclerViewAdapter.addLoadingFooter();
+        } else {
+            followersRecyclerViewAdapter.removeLoadingFooter();
+        }
+    }
+
+    @Override
+    public void setCurrentUser(User user) {
+        Intent intent = new Intent(getContext(), MainActivity.class);
+        intent.putExtra(MainActivity.CURRENT_USER_KEY, user);
+        startActivity(intent);
+    }
+
+    @Override
+    public void displayMessage(String message) {
+        Toast.makeText(getContext(), message, Toast.LENGTH_LONG).show();
     }
 
     /**
@@ -113,11 +143,7 @@ public class FollowersFragment extends Fragment {
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    GetUserTask getUserTask = new GetUserTask(Cache.getInstance().getCurrUserAuthToken(),
-                            userAlias.getText().toString(), new GetUserHandler());
-                    ExecutorService executor = Executors.newSingleThreadExecutor();
-                    executor.execute(getUserTask);
-                    Toast.makeText(getContext(), "Getting user's profile...", Toast.LENGTH_LONG).show();
+                    presenter.onFollowerItemClick(userAlias.getText().toString());
                 }
             });
         }
@@ -136,34 +162,6 @@ public class FollowersFragment extends Fragment {
             Picasso.get().load(user.getImageUrl()).into(userImage);
 
         }
-
-        /**
-         * Message handler (i.e., observer) for GetUserTask.
-         */
-        private class GetUserHandler extends Handler {
-
-            public GetUserHandler() {
-                super(Looper.getMainLooper());
-            }
-
-            @Override
-            public void handleMessage(@NonNull Message msg) {
-                boolean success = msg.getData().getBoolean(GetUserTask.SUCCESS_KEY);
-                if (success) {
-                    User user = (User) msg.getData().getSerializable(GetUserTask.USER_KEY);
-
-                    Intent intent = new Intent(getContext(), MainActivity.class);
-                    intent.putExtra(MainActivity.CURRENT_USER_KEY, user);
-                    startActivity(intent);
-                } else if (msg.getData().containsKey(GetUserTask.MESSAGE_KEY)) {
-                    String message = msg.getData().getString(GetUserTask.MESSAGE_KEY);
-                    Toast.makeText(getContext(), "Failed to get user's profile: " + message, Toast.LENGTH_LONG).show();
-                } else if (msg.getData().containsKey(GetUserTask.EXCEPTION_KEY)) {
-                    Exception ex = (Exception) msg.getData().getSerializable(GetUserTask.EXCEPTION_KEY);
-                    Toast.makeText(getContext(), "Failed to get user's profile because of exception: " + ex.getMessage(), Toast.LENGTH_LONG).show();
-                }
-            }
-        }
     }
 
     /**
@@ -177,13 +175,6 @@ public class FollowersFragment extends Fragment {
 
         private boolean hasMorePages;
         private boolean isLoading = false;
-
-        /**
-         * Creates an instance and loads the first page of following data.
-         */
-        FollowersRecyclerViewAdapter() {
-            loadMoreItems();
-        }
 
         /**
          * Adds new users to the list from which the RecyclerView retrieves the users it displays
@@ -286,15 +277,7 @@ public class FollowersFragment extends Fragment {
          * data.
          */
         void loadMoreItems() {
-            if (!isLoading) {   // This guard is important for avoiding a race condition in the scrolling code.
-                isLoading = true;
-                addLoadingFooter();
-
-                GetFollowersTask getFollowersTask = new GetFollowersTask(Cache.getInstance().getCurrUserAuthToken(),
-                        user, PAGE_SIZE, lastFollower, new GetFollowersHandler());
-                ExecutorService executor = Executors.newSingleThreadExecutor();
-                executor.execute(getFollowersTask);
-            }
+            presenter.loadMoreItems(user);
         }
 
         /**
