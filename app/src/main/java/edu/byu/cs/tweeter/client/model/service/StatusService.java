5815d4082d25d32069224c4a48cfc47bd73452bb
Taylor Brown <4754387+skylon07@users.noreply.github.com>
2023-09-27 20:40:31 -0600

    Add getter for story (generic "more items" --> get feed)

--- app/src/main/java/edu/byu/cs/tweeter/client/model/service/StatusService.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/model/service/StatusService.java
@@ -11,12 +11,13 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import edu.byu.cs.tweeter.client.model.service.backgroundTask.GetFeedTask;
+import edu.byu.cs.tweeter.client.model.service.backgroundTask.GetStoryTask;
 import edu.byu.cs.tweeter.model.domain.AuthToken;
 import edu.byu.cs.tweeter.model.domain.Status;
 import edu.byu.cs.tweeter.model.domain.User;
 
 public class StatusService {
-    public void loadMoreItems(AuthToken authToken, User user, int pageSize, Status lastStatus, Observer observer) {
+    public void loadFeed(AuthToken authToken, User user, int pageSize, Status lastStatus, Observer observer) {
         GetFeedTask getFeedTask = new GetFeedTask(
             authToken,
             user,
@@ -26,6 +27,18 @@ public class StatusService {
         );
         ExecutorService executor = Executors.newSingleThreadExecutor();
         executor.execute(getFeedTask);
+    }
+
+    public void loadStory(AuthToken authToken, User user, int pageSize, Status lastStatus, Observer observer) {
+        GetStoryTask getStoryTask = new GetStoryTask(
+            authToken,
+            user,
+            pageSize,
+            lastStatus,
+            new GetStoryHandler(observer)
+        );
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        executor.execute(getStoryTask);
     }
 
     public interface Observer {
@@ -61,4 +74,33 @@ public class StatusService {
             }
         }
     }
+
+    /**
+     * Message handler (i.e., observer) for GetStoryTask.
+     */
+    private class GetStoryHandler extends Handler {
+        private final Observer observer;
+
+        public GetStoryHandler(Observer observer) {
+            super(Looper.getMainLooper());
+            this.observer = observer;
+        }
+
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            boolean success = msg.getData().getBoolean(GetStoryTask.SUCCESS_KEY);
+            if (success) {
+                List<Status> statuses = (List<Status>) msg.getData().getSerializable(GetStoryTask.STATUSES_KEY);
+                boolean hasMorePages = msg.getData().getBoolean(GetStoryTask.MORE_PAGES_KEY);
+                observer.onItemsLoaded(statuses, hasMorePages);
+            } else if (msg.getData().containsKey(GetStoryTask.MESSAGE_KEY)) {
+                String message = msg.getData().getString(GetStoryTask.MESSAGE_KEY);
+                observer.displayError(message);
+            } else if (msg.getData().containsKey(GetStoryTask.EXCEPTION_KEY)) {
+                Exception ex = (Exception) msg.getData().getSerializable(GetStoryTask.EXCEPTION_KEY);
+                observer.displayException(ex);
+            }
+        }
+    }
+
 }
