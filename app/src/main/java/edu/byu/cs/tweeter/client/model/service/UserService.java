25e066176f09f24296107ea61bcff47e08dd1ddd
Taylor Brown <4754387+skylon07@users.noreply.github.com>
2023-09-27 22:48:13 -0600

    Implement register functionality

--- app/src/main/java/edu/byu/cs/tweeter/client/model/service/UserService.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/model/service/UserService.java
@@ -1,16 +1,20 @@
 package edu.byu.cs.tweeter.client.model.service;
 
+import android.graphics.Bitmap;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
 
 import androidx.annotation.NonNull;
 
+import java.io.ByteArrayOutputStream;
+import java.util.Base64;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import edu.byu.cs.tweeter.client.model.service.backgroundTask.GetUserTask;
 import edu.byu.cs.tweeter.client.model.service.backgroundTask.LoginTask;
+import edu.byu.cs.tweeter.client.model.service.backgroundTask.RegisterTask;
 import edu.byu.cs.tweeter.model.domain.AuthToken;
 import edu.byu.cs.tweeter.model.domain.User;
 
@@ -35,8 +39,25 @@ public class UserService {
         executor.execute(loginTask);
     }
 
-    public void registerUser(String userAlias, String password) {
+    public void registerUser(String firstName, String lastName, String userAlias, String password, Bitmap imageToUpload, LoginObserver observer) {
+        // Convert image to byte array.
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        imageToUpload.compress(Bitmap.CompressFormat.JPEG, 100, bos);
+        byte[] imageBytes = bos.toByteArray();
+        // Intentionally, Use the java Base64 encoder so it is compatible with M4.
+        String imageBytesBase64 = Base64.getEncoder().encodeToString(imageBytes);
 
+        RegisterTask registerTask = new RegisterTask(
+            firstName,
+            lastName,
+            userAlias,
+            password,
+            imageBytesBase64,
+            new RegisterHandler(observer)
+        );
+
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        executor.execute(registerTask);
     }
 
     public interface Observer {
@@ -101,6 +122,33 @@ public class UserService {
                 observer.displayError(message);
             } else if (msg.getData().containsKey(LoginTask.EXCEPTION_KEY)) {
                 Exception ex = (Exception) msg.getData().getSerializable(LoginTask.EXCEPTION_KEY);
+                observer.displayException(ex);
+            }
+        }
+    }
+
+    // RegisterHandler
+
+    private class RegisterHandler extends Handler {
+        private final LoginObserver observer;
+
+        public RegisterHandler(LoginObserver observer) {
+            super(Looper.getMainLooper());
+            this.observer = observer;
+        }
+
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            boolean success = msg.getData().getBoolean(RegisterTask.SUCCESS_KEY);
+            if (success) {
+                User registeredUser = (User) msg.getData().getSerializable(RegisterTask.USER_KEY);
+                AuthToken authToken = (AuthToken) msg.getData().getSerializable(RegisterTask.AUTH_TOKEN_KEY);
+                observer.onUserLoggedIn(registeredUser, authToken);
+            } else if (msg.getData().containsKey(RegisterTask.MESSAGE_KEY)) {
+                String message = msg.getData().getString(RegisterTask.MESSAGE_KEY);
+                observer.displayError(message);
+            } else if (msg.getData().containsKey(RegisterTask.EXCEPTION_KEY)) {
+                Exception ex = (Exception) msg.getData().getSerializable(RegisterTask.EXCEPTION_KEY);
                 observer.displayException(ex);
             }
         }
