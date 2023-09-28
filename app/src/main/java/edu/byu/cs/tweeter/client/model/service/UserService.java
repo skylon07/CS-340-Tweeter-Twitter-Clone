edd27f53701cd052f3ccfdfedb8b966993149bfe
Taylor Brown <4754387+skylon07@users.noreply.github.com>
2023-09-27 21:50:03 -0600

    Implement login functionality

--- app/src/main/java/edu/byu/cs/tweeter/client/model/service/UserService.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/model/service/UserService.java
@@ -1,15 +1,21 @@
 package edu.byu.cs.tweeter.client.model.service;
 
+import android.content.Intent;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import edu.byu.cs.tweeter.client.cache.Cache;
 import edu.byu.cs.tweeter.client.model.service.backgroundTask.GetUserTask;
+import edu.byu.cs.tweeter.client.model.service.backgroundTask.LoginTask;
+import edu.byu.cs.tweeter.client.view.login.LoginFragment;
+import edu.byu.cs.tweeter.client.view.main.MainActivity;
 import edu.byu.cs.tweeter.model.domain.AuthToken;
 import edu.byu.cs.tweeter.model.domain.User;
 
@@ -24,8 +30,24 @@ public class UserService {
         executor.execute(getUserTask);
     }
 
+    public void loginUser(String userAlias, String password, LoginObserver observer) {
+        LoginTask loginTask = new LoginTask(
+            userAlias,
+            password,
+            new LoginHandler(observer)
+        );
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        executor.execute(loginTask);
+    }
+
     public interface Observer {
         void onUserLoaded(User user);
+        void displayError(String message);
+        void displayException(Exception ex);
+    }
+
+    public interface LoginObserver {
+        void onUserLoggedIn(User user, AuthToken authToken);
         void displayError(String message);
         void displayException(Exception ex);
     }
@@ -52,6 +74,34 @@ public class UserService {
                 observer.displayError(message);
             } else if (msg.getData().containsKey(GetUserTask.EXCEPTION_KEY)) {
                 Exception ex = (Exception) msg.getData().getSerializable(GetUserTask.EXCEPTION_KEY);
+                observer.displayException(ex);
+            }
+        }
+    }
+
+    /**
+     * Message handler (i.e., observer) for LoginTask
+     */
+    private class LoginHandler extends Handler {
+        private final LoginObserver observer;
+
+        public LoginHandler(LoginObserver observer) {
+            super(Looper.getMainLooper());
+            this.observer = observer;
+        }
+
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            boolean success = msg.getData().getBoolean(LoginTask.SUCCESS_KEY);
+            if (success) {
+                User loggedInUser = (User) msg.getData().getSerializable(LoginTask.USER_KEY);
+                AuthToken authToken = (AuthToken) msg.getData().getSerializable(LoginTask.AUTH_TOKEN_KEY);
+                observer.onUserLoggedIn(loggedInUser, authToken);
+            } else if (msg.getData().containsKey(LoginTask.MESSAGE_KEY)) {
+                String message = msg.getData().getString(LoginTask.MESSAGE_KEY);
+                observer.displayError(message);
+            } else if (msg.getData().containsKey(LoginTask.EXCEPTION_KEY)) {
+                Exception ex = (Exception) msg.getData().getSerializable(LoginTask.EXCEPTION_KEY);
                 observer.displayException(ex);
             }
         }
