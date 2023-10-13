7594556f91caf5a5d00fd041e8c20fe3f72ee20d
Taylor Brown <4754387+skylon07@users.noreply.github.com>
2023-10-13 14:13:28 -0600

    Remove unused imports

--- app/src/main/java/edu/byu/cs/tweeter/client/presenter/main/MainPresenter.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/presenter/main/MainPresenter.java
@@ -4,8 +4,6 @@ import edu.byu.cs.tweeter.client.cache.Cache;
 import edu.byu.cs.tweeter.client.model.service.FollowService;
 import edu.byu.cs.tweeter.client.model.service.StatusService;
 import edu.byu.cs.tweeter.client.model.service.UserService;
-import edu.byu.cs.tweeter.client.model.service.observer.ResultObserver;
-import edu.byu.cs.tweeter.client.model.service.observer.SuccessObserver;
 import edu.byu.cs.tweeter.client.presenter.BasePresenter;
 import edu.byu.cs.tweeter.model.domain.AuthToken;
 import edu.byu.cs.tweeter.model.domain.User;
