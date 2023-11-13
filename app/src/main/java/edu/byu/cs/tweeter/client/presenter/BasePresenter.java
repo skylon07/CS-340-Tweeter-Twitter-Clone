5ed07c1d523180c6f396a3211a4719ce21537fd6
Taylor Brown <4754387+skylon07@users.noreply.github.com>
2023-11-12 23:43:00 -0700

    Improve presenter observer heirarchy (just something I noticed)

--- app/src/main/java/edu/byu/cs/tweeter/client/presenter/BasePresenter.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/presenter/BasePresenter.java
@@ -1,6 +1,7 @@
 package edu.byu.cs.tweeter.client.presenter;
 
 import edu.byu.cs.tweeter.client.model.service.observer.ResultObserver;
+import edu.byu.cs.tweeter.client.model.service.observer.ServiceObserver;
 import edu.byu.cs.tweeter.client.model.service.observer.SuccessObserver;
 
 public abstract class BasePresenter<ViewType extends BasePresenter.View> {
@@ -14,12 +15,10 @@ public abstract class BasePresenter<ViewType extends BasePresenter.View> {
         void displayMessage(String message);
     }
 
-    protected abstract class ServiceResultObserver<ResultType> implements ResultObserver<ResultType> {
-        private final String actionTag;
+    private abstract class BaseServiceObserver implements ServiceObserver {
+        protected final String actionTag;
 
-        public ServiceResultObserver(String actionTag) {
-            this.actionTag = actionTag;
-        }
+        public BaseServiceObserver(String actionTag) { this.actionTag = actionTag; }
 
         @Override
         public void handleFailure(String message) {
@@ -32,21 +31,15 @@ public abstract class BasePresenter<ViewType extends BasePresenter.View> {
         }
     }
 
-    protected abstract class ServiceSuccessObserver implements SuccessObserver {
-        private final String actionTag;
+    protected abstract class ServiceResultObserver<ResultType> extends BaseServiceObserver implements ResultObserver<ResultType> {
+        public ServiceResultObserver(String actionTag) {
+            super(actionTag);
+        }
+    }
 
+    protected abstract class ServiceSuccessObserver extends BaseServiceObserver implements SuccessObserver {
         public ServiceSuccessObserver(String actionTag) {
-            this.actionTag = actionTag;
-        }
-
-        @Override
-        public void handleFailure(String message) {
-            view.displayMessage("Failed to " + actionTag + ": " + message);
-        }
-
-        @Override
-        public void handleException(Exception exception) {
-            view.displayMessage("Failed to " + actionTag + " because of exception: " + exception.getMessage());
+            super(actionTag);
         }
     }
 }
